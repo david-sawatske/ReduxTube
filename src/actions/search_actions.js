bb3160badecfd4816e64097c6a8e6a971963b3f1
David Sawatske <sawatskeda10@gmail.com>
2018-04-19 19:14:09 -0500

    Write Acton Creators that use the search helper fn to fetch YT data

---
+++ src/actions/search_actions.js
@@ -0,0 +1,20 @@
+import * as YouTube from '../util/youtube_api_util';
+
+export const START_VIDEO_SEARCH_FETCH = 'START_VIDEO_SEARCH_FETCH';
+export const RECEIVE_VIDEO_SEARCH = 'RECEIVE_VIDEO_SEARCH';
+
+export const receiveVideoSearch = videos => ({
+  type: RECEIVE_VIDEO_SEARCH,
+  searchResults: videos
+});
+
+export const startVideoSearchFetch = () => ({
+  type: START_VIDEO_SEARCH_FETCH
+});
+
+export const requestVideoSearch = term => dispatch => {
+  dispatch(startVideoSearchFetch());
+  return YouTube.fetchVideoSearch(term).then(videos => {
+    dispatch(receiveVideoSearch(videos))
+  });
+};
