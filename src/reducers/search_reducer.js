44b29e8822d88fd1eac24c4045f2bf904d6210ab
David Sawatske <sawatskeda10@gmail.com>
2018-04-19 19:44:26 -0500

    Include ADD and REMOVE actions to SearchReducer to remove duplications

--- src/reducers/search_reducer.js
+++ src/reducers/search_reducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 
+import { VIDEO_REMOVE, VIDEO_ADD  } from '../actions/playlist_actions';
 import { RECEIVE_VIDEO_SEARCH } from '../actions/search_actions';
 
 import { merge, union } from 'lodash';
@@ -8,6 +9,10 @@ const searchedById = (state = {}, action) => {
   switch (action.type) {
   case RECEIVE_VIDEO_SEARCH:
     return merge({}, getSearchData(action.searchResults.items).byId)
+  case VIDEO_ADD:
+    return state.filter( vid => vid != action.video);
+  case VIDEO_REMOVE:
+    return [...new Set([action.video, ...state])];
   default:
     return state;
   }
