2abc8f1137a52f4e73799a7c5fcae45d58a53411
David Sawatske <sawatskeda10@gmail.com>
2018-04-23 08:11:15 -0500

    Create and add App logo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import PlaylistShow from './playist/playlist_show_container';
+import Header from './header/app-header';
 
 export default class App extends Component {
   render() {
     return (
       <div className="app-container">
+        <Header />
         <PlaylistShow />
       </div>
     );
---
+++ src/components/header/app-header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+  <div className='header'>
+
+    <img src={'http://res.cloudinary.com/sawatskeda10/image/upload/v1524488678/reduxTube_tdm9bx.png'}
+                   className='play'
+                   alt='play'/>
+  </div>
+);
+
+export default Header;
